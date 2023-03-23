fd60c0a6261109e7bd6198ac853b70c261a8d5c3
dhw <neelydhw@hotmail.com>
2023-03-23 15:01:17 +0800

    Initial Commit

---
+++ miniprogram/api/request.ts
@@ -0,0 +1,26 @@
+const baseUrl = "https://dhw-music-38234-8-1317417350.sh.run.tcloudbase.com";
+export const get = (uri:string) => {
+  wx.showLoading({
+    title:"加载中"
+  })
+  return new Promise((resolve,reject)=>{
+    wx.request({
+      url:baseUrl + uri,
+      method:'GET',
+      success: (res)=>{
+        //handleHttpError(res,reject);
+        resolve(res.data);
+      },
+      fail:reject,
+      complete:()=>{
+        wx.hideLoading();
+      }
+    })
+  });
+}
+
+// const handleHttpError = (response,reject) => {
+//   if(response !== 200){
+//     reject("请求异常");
+//   }
+// }
